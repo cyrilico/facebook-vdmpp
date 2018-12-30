a47c83b26a340906232fb8bec07f0c6249cbb1e1
darksystem <diogo.rey@hotmail.com>
2018-12-30 23:18:47 +0000

    Implemented Timeline Menu

--- Java/src/cli/TimelineMenu.java
+++ Java/src/cli/TimelineMenu.java
@@ -1,5 +1,9 @@
 package cli;
 
+import org.overture.codegen.runtime.VDMSeq;
+import vdm.Publication;
+import vdm.User;
+
 import java.util.ArrayList;
 
 class TimelineMenu extends AbstractMenu {
@@ -15,13 +19,13 @@ class TimelineMenu extends AbstractMenu {
         printDivision("Timeline Menu");
 
         ArrayList<String> options = new ArrayList<>();
-        options.add("See My Timeline");
+        options.add("See Main Feed");
         options.add("See Timeline from other users");
 
         String input = printAndSelectOptions(options);
         switch (input) {
-            case "See My Timeline":
-                seeMyTimeline();
+            case "See Main Feed":
+                seeMainFeed();
                 break;
             case "See Timeline from other users":
                 seeTimelineByUser();
@@ -34,9 +38,39 @@ class TimelineMenu extends AbstractMenu {
         getOptions();
     }
 
-    private void seeMyTimeline() {
+    private void seeMainFeed() {
+        VDMSeq mainFeed = mainMenu.facebook.getUserFeed(mainMenu.user);
+        for (Object p1 : mainFeed) {
+            Publication post = (Publication) p1;
+            System.out.println("Post " + post.getId() + "| Author: " + post.getAuthor().getName() + "| Date: " + post.getTimestamp() + "| Likes: " + post.getLikes().size());
+            System.out.println("Content " + post.getContent());
+        }
     }
 
     private void seeTimelineByUser() {
+        User user2 = getUser();
+        VDMSeq timelineByUser = mainMenu.facebook.getUserTimeline(mainMenu.user, user2);
+        for (Object p1 : timelineByUser) {
+            Publication post = (Publication) p1;
+            System.out.println("Post " + post.getId() + "| Author: " + post.getAuthor().getName() + "| Date: " + post.getTimestamp() + "| Likes: " + post.getLikes().size());
+            System.out.println("Content " + post.getContent());
+        }
+    }
+
+    //TODO: Extract this method
+    private User getUser() {
+        User user;
+        int i = 3;
+
+        do {
+            System.out.print("Enter username: ");
+            String username = scanner.nextLine();
+            user = mainMenu.facebook.getUserByName(username);
+            if (user == null)
+                System.out.println("User not found");
+            i--;
+        } while (user == null && i > 0);
+
+        return user;
     }
 }
