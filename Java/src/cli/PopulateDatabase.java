bcdd064685dc1cbb4f411aea0af0939220c0b198
diogotorres97 <diogo.rey@hotmail.com>
2018-12-31 15:04:45 +0000

    Update PopulateDatabase.java

--- Java/src/cli/PopulateDatabase.java
+++ Java/src/cli/PopulateDatabase.java
@@ -17,7 +17,6 @@ class PopulateDatabase {
     void addDatabase() {
         addUsers();
         addFriendship();
-        addBlockedUsers();
         addPosts();
         addLikes();
         addChats();
